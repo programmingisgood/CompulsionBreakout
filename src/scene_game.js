152b0584651a31d41695d5d99d89f3fbe64f90f4
Brian Cronin <programmingisgood@gmail.com>
2013-07-15 18:13:57 -0700

    Hooked up level meter to XP

--- src/scene_game.js
+++ src/scene_game.js
@@ -7,7 +7,19 @@ var kBallWidth = 8
 var kBallHalfWidth = kBallWidth / 2
 var kBallHeight = 8
 
-function CreateBlocks(numWide, numHigh)
+/*var kAchievements = { };
+kAchievements["numHits"] = { }
+kAchievements["numHits"][5] = { "%d hits! Good job!" }
+kAchievements["numHits"][10] = { "%d hits! Good job!" }
+kAchievements["numHits"][20] = { "%d hits! Good job!" }
+kAchievements["numHits"][50] = { "%d hits! Good job!" }
+kAchievements["numHits"][100] = { "%d hits! Good job!" }
+kAchievements["numHits"][150] = { "%d hits! Good job!" }
+kAchievements["numHits"][250] = { "%d hits! Good job!" }
+kAchievements["numHits"][300] = { "%d hits! Good job!" }
+kAchievements["numHits"][350] = { "%d hits! Good job!" }*/
+
+function CreateBlocks(numWide, numHigh, AddXP)
 {
     var blockWidth = (Game.width() / numWide) - 2;
     var blockHeight = (Game.height() / 4 / numHigh) - 2;
@@ -19,6 +31,12 @@ function CreateBlocks(numWide, numHigh)
                         .attr({ x: 1 + w * (blockWidth + 2), y: 1 + h * (blockHeight + 2), w: blockWidth, h: blockHeight })
                         .color("#333399")
                         .collision();
+
+            block.onHit("Collision",
+                function(hitEnts)
+                {
+                    AddXP(5);
+                });
         }
     }
 }
@@ -47,6 +65,9 @@ Crafty.scene('Game', function()
         });
 
     var currentLevel = 1;
+    var xp = 0;
+    var nextLevelXP = 2;
+
     var levelText = Crafty.e("2D, DOM, Text, Color, Tweener")
                     .attr({ x: 10, y: 300, z: 1, w: 120 })
                     .text(function() { return "Level " + currentLevel; })
@@ -61,6 +82,20 @@ Crafty.scene('Game', function()
     var levelBarFG = Crafty.e("2D, Canvas, Color")
                      .attr({ x: levelText.x, y: levelText.y + 8, z: 1, w: 0, h: 16 })
                      .color("#00EE00");
+
+    function AddXP(addxp)
+    {
+        xp += addxp;
+        while (xp >= nextLevelXP)
+        {
+            currentLevel++;
+            xp -= nextLevelXP;
+            nextLevelXP *= nextLevelXP;
+        }
+        levelBarFG.w = 100 * (xp / nextLevelXP);
+
+        levelText.text("Level " + currentLevel);
+    }
 
     var ball = Crafty.e("2D, Canvas, Color, Collision")
                 .attr({ x: Game.width() / 2 - kBallHalfWidth, y: Game.height() / 2, w: kBallWidth, h: kBallHeight })
@@ -81,16 +116,19 @@ Crafty.scene('Game', function()
             {
                 this.x = 0;
                 this.vel.x = -this.vel.x;
+                AddXP(1);
             }
             if (this.x > Game.width() - this.w)
             {
                 this.x = Game.width() - this.w;
                 this.vel.x = -this.vel.x;
+                AddXP(1);
             }
             if (this.y < 0)
             {
                 this.y = 0;
                 this.vel.y = -this.vel.y;
+                AddXP(1);
             }
         });
 
@@ -104,11 +142,13 @@ Crafty.scene('Game', function()
             }
 
             ball.vel.y = -ball.vel.y;
+
+            AddXP(1);
         });
 
     var numWide = 2;
     var numHigh = 2;
-    CreateBlocks(numWide, numHigh);
+    CreateBlocks(numWide, numHigh, AddXP);
 
     Crafty.bind("EnterFrame",
         function()
@@ -117,7 +157,8 @@ Crafty.scene('Game', function()
             {
                 numWide = Math.min(100, numWide + 1);
                 numHigh = Math.min(100, numHigh + 1);
-                CreateBlocks(numWide, numHigh);
+                CreateBlocks(numWide, numHigh, AddXP);
+                AddXP(10);
             }
         })
 });
